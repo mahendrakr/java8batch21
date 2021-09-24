ee34372299a0300f8872ee03d4af906ce5a38459
mahendrakr <mahendrakumar7028@gmail.com>
2021-09-24 21:39:04 +0530

    Lecture1_methods & variables

---
+++ java8/src/javabasics/Students.java
@@ -0,0 +1,19 @@
+package javabasics;
+
+public class Students {
+	String name;
+	int age;
+	String rollNo;
+	long contact;
+	String address;
+	String department;
+	String email;
+	void setName(String name) {}
+	String getName(){
+		return "Aadi";
+	}
+	int getAge() {
+		return 10;
+	}
+	void setAge(int age) {}
+}
