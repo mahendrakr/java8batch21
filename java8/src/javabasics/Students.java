6a37d00a71ae5f825b5b1e5ad3f2c92ad03a202b
mahendrakr <mahendrakumar7028@gmail.com>
2021-09-27 22:05:36 +0530

    lecture 2 calling variable 27-09-21

--- java8/src/javabasics/Students.java
+++ java8/src/javabasics/Students.java
@@ -8,12 +8,27 @@ public class Students {
 	String address;
 	String department;
 	String email;
-	void setName(String name) {}
+	void setName(String name) {
+		System.out.println(name);
+	}
 	String getName(){
 		return "Aadi";
 	}
 	int getAge() {
 		return 10;
 	}
-	void setAge(int age) {}
+	void setAge(int age) {
+		System.out.println(age);
+	}
+	public static void main(String[] args) {
+		Students s = new Students();
+		String name = s.getName();
+		System.out.println(name);
+		int age = s.getAge();
+		System.out.println(age);
+		s.setName("Anamika");
+		s.setAge(20);
+		String email = s.email;
+		System.out.println(email);
+	}
 }
