cabfa48b00cfd5894e3d4a123051a4cae0219ec9
mahendrakr <mahendrakumar7028@gmail.com>
2021-10-06 21:17:13 +0530

    committed by himani

---
+++ java8/src/javabasics/InstanceV.java
@@ -0,0 +1,32 @@
+package javabasics;
+
+public class InstanceV {
+	String name ="Himani";
+	int id=20;
+	int a = 30;
+	int b = 40;
+	int c = 50;
+	int d = 60;
+	int e = 70;
+	static int x;
+	static int y;
+	public String getName() {
+		return "sharma";
+    }
+	public int getId() {
+		System.out.println(x);
+		return 10;		
+	}
+	public static int getX() {
+		System.out.println(a);
+		return 100;
+	}
+	public 
+public static void main(String[] args) {
+	//int result1 = InstanceV.a;
+	//int result2 = InstanceV.b
+			
+	
+	
+}
+}
