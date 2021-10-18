e67c342d5a4b68958ad7c65a59277927c7749d01
mahendrakr <mahendrakumar7028@gmail.com>
2021-10-18 21:26:35 +0530

    Committed by : yashvani verma

---
+++ java8/src/pack1/Dog.java
@@ -0,0 +1,13 @@
+package pack1;
+
+public class Dog {
+	public int age=15;
+	public static int leg=4;
+	public void m1() {
+		System.out.println("m1");
+	}
+	public static void m2() {
+		System.out.println("m2");
+	}
+
+}
