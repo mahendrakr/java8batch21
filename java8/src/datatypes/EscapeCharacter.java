783ee23bc43dbd9f083cfc233332ef02b17ad843
mahendrakr <mahendrakumar7028@gmail.com>
2021-10-21 21:38:34 +0530

    Committed by : Yashvani Verma

---
+++ java8/src/datatypes/EscapeCharacter.java
@@ -0,0 +1,13 @@
+package datatypes;
+
+public class EscapeCharacter {
+
+	public static void main(String[] args) {
+		System.out.println("\"Yashvani\"");
+		System.out.println("\'Ankush\'");
+		System.out.println("\\");
+		System.out.println("Shreya\tSingh");
+
+	}
+
+}
--- java8/src/javabasics/Range.java
+++ java8/src/javabasics/Range.java
@@ -5,6 +5,8 @@ public class Range {
 		 System.out.println(Byte.MIN_VALUE+"  " + Byte.MAX_VALUE);
 		 System.out.println(Short.MIN_VALUE+"  "+Short.MAX_VALUE);
 		 System.out.println(Integer.MIN_VALUE+"  "+Integer.MAX_VALUE);
+		 System.out.println(Long.MIN_VALUE+"  "+Long.MAX_VALUE);
+	
 	}
 
 }
