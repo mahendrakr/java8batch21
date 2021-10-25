ede39399e7c29344b480e5fbf93cdbca36ef5e43
mahendrakr <mahendrakumar7028@gmail.com>
2021-10-25 21:41:37 +0530

    lecture 25-10-21 Operators / increment commit by AADI

---
+++ java8/src/datatypes/WrraperClass.java
@@ -0,0 +1,10 @@
+package datatypes;
+//pending
+public class WrraperClass {
+
+	public static void main(String[] args) {
+		Integer a = 120;
+		System.out.println(a);
+	}
+
+}
---
+++ java8/src/operator/PrePostIncrement.java
@@ -0,0 +1,13 @@
+package operator;
+
+public class PrePostIncrement {
+
+	public static void main(String[] args) {
+		int a = 10;
+	//	++a; //post
+		System.out.println(++a);
+		//++a; //pre
+		
+	}
+
+}
