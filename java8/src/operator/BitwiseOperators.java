0971ec05887edd8e4e8627685a14b15608f171d2
mahendrakr <mahendrakumar7028@gmail.com>
2021-10-29 21:59:38 +0530

    29-10-2021 operators by Ankush pandagre

---
+++ java8/src/operator/BitwiseOperators.java
@@ -0,0 +1,19 @@
+package operator;
+
+public class BitwiseOperators {
+
+	public static void main(String[] args) {
+    System.out.println(4 & 5);
+    System.out.println(5 & 6);
+    System.out.println(4 | 5);
+    System.out.println(5 | 6);
+    System.out.println(4 ^ 5);
+    System.out.println(5 ^ 6);
+    
+    System.out.println(true && false);
+    
+    
+    
+	}
+
+}
---
+++ java8/src/operator/TildOperator.java
@@ -0,0 +1,10 @@
+package operator;
+
+public class TildOperator {
+
+	public static void main(String[] args) {
+    System.out.println(~4);
+    System.out.println(~-4);
+	}
+
+}
