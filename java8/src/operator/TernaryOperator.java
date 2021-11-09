2fd60c7da7d755d1a5f0a805b4dc9970e37d5766
mahendrakr <mahendrakumar7028@gmail.com>
2021-11-09 21:17:30 +0530

    Committed by :Yashvani Verma

---
+++ java8/src/operator/TernaryOperator.java
@@ -0,0 +1,17 @@
+package operator;
+
+public class TernaryOperator {
+
+	public static void main(String[] args) {
+    String s=10>5?"Anushka":"Ankush";
+    System.out.println(s);
+    boolean b= 10<5?true:false;
+    char ch=10==10?'Y':'F'; 
+    System.out.println(ch);
+    String r=20<30 ?(10>5?"Addy":"Annu") :"Anushka";
+    System.out.println(r);
+    
+    
+	}
+
+}
