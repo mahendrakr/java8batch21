0e6e0de6b5e026bc6b05937a2e95026ef31bfb42
mahendrakr <mahendrakumar7028@gmail.com>
2021-11-16 21:25:08 +0530

    Committed by: YAshvani Verma

---
+++ java8/src/controflow/SwitchCase.java
@@ -0,0 +1,19 @@
+package controflow;
+
+public class SwitchCase {
+
+	public static void main(String[] args) {
+    //Rules
+		byte b=40;
+		final byte c=20;
+		switch(b) {
+		case 10:System.out.println("Yashvani");
+		case c:System.out.println("Ankush");
+		case 30:System.out.println("Kavya");
+		case 40:System.out.println("Anushka");
+		default :System.out.println("Addy");
+		
+		}
+	}
+
+}
