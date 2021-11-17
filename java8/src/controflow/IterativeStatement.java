5ca242c6026082337dbcd6a471c333499d75a4e1
mahendrakr <mahendrakumar7028@gmail.com>
2021-11-17 21:39:47 +0530

    lecture 17-11-21 / iteration

---
+++ java8/src/controflow/IterativeStatement.java
@@ -0,0 +1,17 @@
+package controflow;
+
+public class IterativeStatement {
+	public static void main(String[] args) {
+		System.out.println("Kavya"); 
+		System.out.println("Kavya"); 
+		System.out.println("Kavya"); 
+		System.out.println("Kavya"); 
+		System.out.println("Kavya"); 
+		
+		System.out.println("********");
+		int i = 0;
+		for(System.out.println("initialisation");i<5;System.out.println(++i)) {
+			System.out.println("Kavya");
+		}
+	}
+}
