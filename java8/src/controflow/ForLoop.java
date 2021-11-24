9547b116f2e7f9bb9307d791de5574846304a496
mahendrakr <mahendrakumar7028@gmail.com>
2021-11-24 21:27:13 +0530

    Committed by :Yashvani Verma

---
+++ java8/src/controflow/ForLoop.java
@@ -0,0 +1,11 @@
+package controflow;
+
+public class ForLoop {
+
+	public static void main(String[] args) {
+		int a=10;int b=5;
+    for( ;a<b;) {}   //; means it will terminate here only
+    	System.out.println("Kavya");
+	}
+
+}
