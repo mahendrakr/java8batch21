6f9c993e48d382d34b7e5326eb2953c79206d0eb
mahendrakr <mahendrakumar7028@gmail.com>
2021-12-01 21:28:03 +0530

    lecture 1 dec 2021 TWODIMENSIONALARRAY Commit by Anamika

---
+++ java8/src/array/TwoDimensionalArray.java
@@ -0,0 +1,27 @@
+package array;
+
+public class TwoDimensionalArray {
+
+	public static void main(String[] args) {
+    int[] a;
+    int []b;
+    int c[];
+    int d [];
+    int[][] dd = new int[3][2];
+    System.out.println(dd.length);
+    dd[1][1] = 60;
+    dd[2][0] = 50;
+    dd[0][1] = 70;
+    dd[0][0] = 40;
+    dd[1][0] = 30;
+    dd[2][1] = 20;
+    System.out.println(dd[0][1]);
+    System.out.println(dd[1][1]);
+    System.out.println(dd[2][0]);
+    System.out.println(dd[1][0]);
+    System.out.println(dd[2][1]);
+    System.out.println(dd[0][0]);
+    
+	}
+
+}
