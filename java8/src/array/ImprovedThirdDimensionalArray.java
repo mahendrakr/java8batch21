f193490778bac4a01999164bd71f439e709a5c99
mahendrakr <mahendrakumar7028@gmail.com>
2021-12-09 19:23:38 +0530

    ImprovedThirdDimensioanlArray 8-12-21 commit by anamika

--- java8/src/array/ImprovedThirdDimensionalArray.java
+++ java8/src/array/ImprovedThirdDimensionalArray.java
@@ -3,7 +3,7 @@ package array;
 public class ImprovedThirdDimensionalArray {
 
 	public static void main(String[] args) {
-    int[][][] a= 
+    int[][][] array= 
     		{//1D Starts
     		
     		{//2D Starts
@@ -15,18 +15,29 @@ public class ImprovedThirdDimensionalArray {
     		}//2D Ends
     };//1D Ends
     //Access the elements 9 , 6 ,2 ,10
-    System.out.println(a[1][1][1]);
-    System.out.println(a[0][1][0]);
-    System.out.println(a[1][0][1]);
-    System.out.println(a[1][2][0]);
-    System.out.println("********************");
-    for(int [] [] i:a) {
+    System.out.println(array[1][1][1]);
+    System.out.println(array[0][1][0]);
+    System.out.println(array[1][0][1]);
+    System.out.println(array[1][2][0]);
+    System.out.println("******Enhanced For Loop**************");
+    for(int [] [] i:array) {
     	for(int [] j:i) {
     		for(int k:j) {
     			System.out.println(k);
     		}
     	}
     }
+    System.out.println("######### Normal For Loop ###########");
+//  using normal for loop
+  for(int i=0;i<array.length;i++) {
+  	for(int j=0;j<array[i].length;j++) {
+  		for(int k=0;k<array[i][j].length;k++) {
+  			System.out.println(array[i][j][k]);
+  		}
+  	}
+  }
 	}
-
+	
 }
+
+
