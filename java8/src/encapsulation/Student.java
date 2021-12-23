b87e6b4b1d3b8695d35ec898fd7c29863335ddf7
mahendrakr <mahendrakumar7028@gmail.com>
2021-12-23 22:05:54 +0530

    Encapsulation (23/12/21) commited by anamika

---
+++ java8/src/encapsulation/Student.java
@@ -0,0 +1,24 @@
+package encapsulation;
+
+public class Student {
+		private String name = "Anushka";
+		private int age= 20;
+		private int ID=123;
+		private long phoneNo=700498812;
+		private String address="Bhopal";
+		public String getName() {
+			return name;
+		}
+		public int getAge() {
+			return age;
+		}
+		public int getID() {
+			return ID;
+		}
+		public long getPhoneNo() {
+			return phoneNo;
+		}
+		public String getAddress() {
+			return address;
+		}
+}
---
+++ java8/src/encapsulation/Test.java
@@ -0,0 +1,16 @@
+package encapsulation;
+
+public class Test {
+	public static void main(String[] args) {
+		Student s =new Student();
+		
+		System.out.println(s.getAddress());
+		System.out.println(s.getAge());
+		System.out.println(s.getID());
+		System.out.println(s.getName());
+		System.out.println(s.getPhoneNo());
+	}
+
+}
+
+
