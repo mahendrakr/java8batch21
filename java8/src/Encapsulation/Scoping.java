0056e5153678e19a45c2228269ae62a47916a383
mahendrakr <mahendrakumar7028@gmail.com>
2021-12-30 22:06:11 +0530

    lecture 30-12-2021 / scoping

---
+++ java8/src/Encapsulation/Scoping.java
@@ -0,0 +1,25 @@
+package Encapsulation;
+/**
+ * Scoping means visibility
+ * Local variable : local variables get memory inside stack.
+ * Method parameters : It get memory inside stack
+ * Only final modifier can be used for method parameters and local variables.
+ * @author adity
+ *
+ */
+public class Scoping {
+	public void doSomething(final int a, String s) {
+		System.out.println(a);
+//		System.out.println(c);
+		final int c=30;
+		System.out.println(c);
+	}
+	public static void main(String[] args) {
+		int a=10;
+		System.out.println(a);
+		int x=20;
+		do {
+		}
+			while(x<30);
+	}
+}
