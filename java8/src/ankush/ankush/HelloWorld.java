7813352a7a54de724a385e76717ccad0a3bd934d
mahendrakr <mahendrakumar7028@gmail.com>
2022-01-05 21:22:09 +0530

    Committed by: Yashvani Verma

--- java8/src/ankush/ankush/HelloWorld.java
+++
@@ -1,7 +0,0 @@
-package ankush;
-
-public class HelloWorld {
-public static void main(String[] args) {
-
-}
-}
---
+++ java8/src/inheritence/Animal.java
@@ -0,0 +1,12 @@
+package inheritence;
+/**
+ * Ques) Can we create an object of abstract class? why?
+ * Ans) No, because abstract is a concept it does not exist in real world 
+ * 
+ * 
+ * @author user
+ *
+ */
+public abstract class Animal {
+	public abstract void speak();
+}
